2cb21a73250f8aad392d311ce73dbb74cd7b8276
Tubalevia <alvaro.prata.filho@gmail.com>
2021-01-30 14:28:56 -0600

    changed tiles folder, added brick_texture, merged

--- scenes/maze_editor/grid.gd
+++ scenes/maze_editor/grid.gd
@@ -5,30 +5,25 @@ func _ready():
     pass
     
 func reset():
-    for x in range(3, 23):
-        for y in range(3, 25):
-            set_cell(x, y, 1)
-            
+	for x in range(3, 23):
+		for y in range(3, 25):
+			set_cell(x, y, 5)
+			
 func submit():
-    emit_signal("coordinates", get_used_cells_by_id(1) + get_used_cells_by_id(0))
+	emit_signal("coordinates", get_used_cells_by_id(5) + get_used_cells_by_id(6))
 
 func _process(delta):
-    var loc = get_mouse_loc()
-    var cell = get_cell(loc.x, loc.y)
-    if Input.is_mouse_button_pressed(1):
-        match cell:
-            -1: pass
-            0: pass
-            1: set_cell(loc.x, loc.y, -1)
-            2: reset()
-            3: submit()
-            4: pass
-            
-        print(cell)
+	if Input.is_mouse_button_pressed(1):
+		var pos = get_viewport().get_mouse_position()
+		var loc = world_to_map(pos)
+		var cell = get_cell(loc.x, loc.y)
+		match cell:
+			5: set_cell(loc.x, loc.y, -1)	
+		print(cell)
     if Input.is_mouse_button_pressed(2):
-        if (cell == -1):
-            set_cell(loc.x, loc.y, 1)
-            
+        if (cell == 7):
+            set_cell(loc.x, loc.y, 5)
+
 func get_mouse_loc():
     var pos = get_viewport().get_mouse_position()
     return world_to_map(pos)
