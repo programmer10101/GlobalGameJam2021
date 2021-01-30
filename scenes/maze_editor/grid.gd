adf6950786d81df6b3e675eeffe4f89c3bb7bef0
Tubalevia <alvaro.prata.filho@gmail.com>
2021-01-30 01:53:35 -0600

    Added signal and self destruction

--- scenes/maze_editor/grid.gd
+++ scenes/maze_editor/grid.gd
@@ -1,4 +1,5 @@
 extends TileMap
+signal coordinates
 
 func _ready():
 	pass
@@ -12,7 +13,7 @@ func _process(delta):
 			-1: pass
 			3: pass
 			1: set_cell(loc.x, loc.y, -1)
-			0: pass # submit get_used_cells_by_id(4)
+			0: emit_signal("coordinates", get_used_cells_by_id(1))
 			2: for x in range(3, 23):
 				for y in range(3, 25):
 					set_cell(x, y, 1)
