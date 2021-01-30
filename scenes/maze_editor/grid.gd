ef9b2a466bf50da1e0568e9c46b55d957bf6b624
Tubalevia <alvaro.prata.filho@gmail.com>
2021-01-30 16:40:55 -0600

    added hover to tiles

--- scenes/maze_editor/grid.gd
+++ scenes/maze_editor/grid.gd
@@ -1,9 +1,30 @@
 extends TileMap
 signal coordinates
+var currentTile = Vector2(13, 3)
 
 func _ready():
     pass
     
+func _input(event):
+	if event is InputEventMouseMotion:
+		var pos = event.position
+		var loc = world_to_map(pos)
+		if insideMaze(loc):
+			var lastCell = get_cell(currentTile.x, currentTile.y)
+			var cell = get_cell(loc.x, loc.y)
+			if(currentTile != loc ): print(lastCell)
+			if(currentTile != loc and lastCell == -1): 
+				set_cellv(currentTile, 5)
+			if(cell == 5 ): 
+				set_cellv(loc, -1)
+			currentTile = loc
+		else:
+			set_cellv(currentTile, 5)
+
+func insideMaze(loc):
+	if (loc.x > 2 and loc.x < 23 and loc.y > 2 and loc.y < 25):
+		return true
+
 func reset():
 	for x in range(3, 23):
 		for y in range(3, 25):
@@ -17,9 +38,8 @@ func _process(delta):
 		var pos = get_viewport().get_mouse_position()
 		var loc = world_to_map(pos)
 		var cell = get_cell(loc.x, loc.y)
-		match cell:
-			5: set_cell(loc.x, loc.y, -1)	
-		print(cell)
+		if(insideMaze(loc) and cell == -1):
+			set_cell(loc.x, loc.y, 7)
     if Input.is_mouse_button_pressed(2):
         if (cell == 7):
             set_cell(loc.x, loc.y, 5)
