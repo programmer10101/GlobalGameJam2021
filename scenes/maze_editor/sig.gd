53310e93b14d99993ea599be0945a6a196ba29bf
Tubalevia <alvaro.prata.filho@gmail.com>
2021-01-30 17:31:01 -0600

    fixed mouse location bug, removed decrease button

--- scenes/maze_editor/sig.gd
+++ scenes/maze_editor/sig.gd
@@ -2,8 +2,8 @@ extends Node2D
 signal coord
 
 func _ready():
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 func _on_TileMap_coordinates(arr):
-    emit_signal("coord", arr)
-    queue_free()
+	emit_signal("coord", arr)
+	queue_free()
