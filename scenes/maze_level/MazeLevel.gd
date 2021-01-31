6b775811d27caa745f1ebf75b6731944318f28a8
Tubalevia <alvaro.prata.filho@gmail.com>
2021-01-31 02:53:07 -0600

    fixed submit button: signal reference

--- scenes/maze_level/MazeLevel.gd
+++ scenes/maze_level/MazeLevel.gd
@@ -8,10 +8,10 @@ extends Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
-    
+	pass # Replace with function body.
+	
 func init(arr):
-    $WorldEnvironment/MultiMeshInstance.init(arr)
+	$WorldEnvironment/MultiMeshInstance.init(arr)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scenes/maze_level/MultiMeshInstance.gd
+++ scenes/maze_level/MultiMeshInstance.gd
@@ -5,43 +5,43 @@ var y_origin = cube_size / 2.0
 var collisionNode = StaticBody.new()
 
 func init(arr):
-    self.add_child(collisionNode)
-    setup_multimesh(arr.size())
-    generate_cubes(arr)
+	self.add_child(collisionNode)
+	setup_multimesh(arr.size())
+	generate_cubes(arr)
 
 func generate_cubes(arr):
-    print(arr)
-    var offset = findOffset(arr)
-    print(offset)
-    for i in range(arr.size()):
-        generate_cube(arr[i], i, offset)
-    
+	print(arr)
+	var offset = findOffset(arr)
+	print(offset)
+	for i in range(arr.size()):
+		generate_cube(arr[i], i, offset)
+	
 func findOffset(arr):
-    var minXY = arr[0]
-    var maxXY = arr[0]
-    for i in arr:
-        minXY.x = min(i.x, minXY.x)
-        minXY.y = min(i.y, minXY.y)
-        maxXY.x = max(i.x, maxXY.x)
-        maxXY.y = max(i.y, maxXY.y)
-    return Vector2((maxXY.x - minXY.x) * cube_size / 2.0, (maxXY.y - minXY.y) * cube_size / 2.0)
+	var minXY = arr[0]
+	var maxXY = arr[0]
+	for i in arr:
+		minXY.x = min(i.x, minXY.x)
+		minXY.y = min(i.y, minXY.y)
+		maxXY.x = max(i.x, maxXY.x)
+		maxXY.y = max(i.y, maxXY.y)
+	return Vector2((maxXY.x - minXY.x) * cube_size / 2.0, (maxXY.y - minXY.y) * cube_size / 2.0)
 
 func generate_cube(loc, i, offset):
-    var pos = Vector3(loc.x * cube_size - offset.x, y_origin, loc.y * cube_size - offset.y)
-    var transform = Transform(Basis(), pos)
-    self.multimesh.set_instance_transform(i, Transform(Basis(), pos))
-    var collisionShape = CollisionShape.new()
-    var mesh = CubeMesh.new()
-    mesh.size = Vector3(1,1,1)
-    collisionShape.shape = mesh
-    collisionShape.transform = transform
-    collisionNode.add_child(collisionShape)
+	var pos = Vector3(loc.x * cube_size - offset.x, y_origin, loc.y * cube_size - offset.y)
+	var transform = Transform(Basis(), pos)
+	self.multimesh.set_instance_transform(i, Transform(Basis(), pos))
+	var collisionShape = CollisionShape.new()
+	var mesh = CubeMesh.new()
+	mesh.size = Vector3(1,1,1)
+	collisionShape.shape = mesh
+	collisionShape.transform = transform
+	collisionNode.add_child(collisionShape)
 
 func setup_multimesh(instances):
-    self.multimesh = MultiMesh.new()
-    self.multimesh.transform_format = MultiMesh.TRANSFORM_3D
-    self.multimesh.instance_count = instances
-    self.multimesh.visible_instance_count = self.multimesh.instance_count
-    var mesh = CubeMesh.new()
-    mesh.size = Vector3(1,1,1)
-    self.multimesh.mesh = mesh
+	self.multimesh = MultiMesh.new()
+	self.multimesh.transform_format = MultiMesh.TRANSFORM_3D
+	self.multimesh.instance_count = instances
+	self.multimesh.visible_instance_count = self.multimesh.instance_count
+	var mesh = CubeMesh.new()
+	mesh.size = Vector3(1,1,1)
+	self.multimesh.mesh = mesh
