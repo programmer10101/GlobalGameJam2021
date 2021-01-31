6b0e038672e08c29b7afdad0c1d5e045ba5ee54b
Tubalevia <alvaro.prata.filho@gmail.com>
2021-01-31 04:22:59 -0600

    added audio files

--- scenes/maze_editor/MazeEditor.gd
+++ scenes/maze_editor/MazeEditor.gd
@@ -3,11 +3,11 @@ signal coord
 signal buildMaze
 
 func _ready():
-    pass # Replace with function body.
-    
+	pass # Replace with function body.
+	
 func _init(x=5, y=10):
-    emit_signal("buildMaze", Vector2(x,y))
+	emit_signal("buildMaze", Vector2(x,y))
 
 func _on_TileMap_coordinates(arr, s, e):
-    emit_signal("coord", arr, s, e)
-    #queue_free()
+	emit_signal("coord", arr, s, e)
+	#queue_free()
